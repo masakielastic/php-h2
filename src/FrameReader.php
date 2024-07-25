ad4f666a4b3a6cffedd7bb8256aa9472b616c5ce
Masaki Kagaya <masakielastic@gmail.com>
2024-07-25 12:59:49 +0900

    added FrameReader

---
+++ src/FrameReader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace h2;
+
+class FrameReader implements \IteratorAggregate {
+
+    public function __construct(readonly string $src)
+    {
+    }
+
+    public function getIterator(): \Generator
+    {
+        $chunk_size = \strlen($this->src);
+        $index = 0;
+        $next = 0;
+
+        while (true) {
+            if ($next > $chunk_size || $next + 3 > $chunk_size) {
+                break;
+            }
+
+            $size = \hexdec(\bin2hex(\substr($this->src, $next, 3))) + 9;
+            $next += $size;
+
+            yield $index => $next;
+            ++$index;
+        }
+    }
+
+}
