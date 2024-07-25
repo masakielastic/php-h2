44cb416f5131f26d7964c2c67b9e8f59891ee31f
Masaki Kagaya <masakielastic@gmail.com>
2024-07-25 17:45:03 +0900

    updated HeadersFrame

--- src/FrameWriter.php
+++ src/FrameWriter.php
@@ -4,7 +4,7 @@ namespace h2;
 
 class FrameWriter implements \IteratorAggregate {
     public $buffer = "";
-    public $list = [];
+    public $frames = [];
 
     function public getIterator():
     {
@@ -17,35 +17,6 @@ class FrameWriter implements \IteratorAggregate {
 
     function writeHeadersFrame(int $streamId, array $headers, array $flags = [])
     {
-        $stream = match(true) {
-            0x100 > $streamId => "\x00\x00\x00",
-            0x10000 > $streamId => "\x00\x00",
-            0x1000000 > $streamId => "\x00",
-            default => ""
-        }.chr($streamId);
-
-        $payload = "";
-
-        foreach ($headers as $header) {
-            $payload .= chr(0x00).pack("c", strlen($header[0])).$header[0].pack("c", strlen($header[1])).$header[1];
-        }
-
-        $length = strlen($payload);
-
-        $payloadLength = match(true) {
-            0x100 > $length => "\x00\x00",
-            0x10000 > $length => "\x00",
-            default => ""
-        }.chr($length);
-
-        $flag = 0;
-
-        if (count($flags)) {
-           foreach ($flags as $value) {
-               $flag += $value;
-           }
-        }
-
-        $this->buffer .= $payloadLength.chr(FrameType::HEADERS).pack("c", $flag).$stream.$payload;
+        $this->frames[] = new Dataframe($streamId, $headers, $flags);
     }
 }
--- src/HeadersFrame.php
+++ src/HeadersFrame.php
@@ -1,6 +1,54 @@
 <?php
 
+namespace h2;
+use h2\FrameType::HEADERS;
+
 class HeadersFrame extends Frame {
     const int FLAG_END_STREAM = 0x1;
     const int FLAG_END_HEADERS = 0x4;
+
+    string $bytes;
+
+    public __construct(int $streamId, array $headers, array $flags = [])
+    {
+        $stream = match(true) {
+            0x100 > $streamId => "\x00\x00\x00",
+            0x10000 > $streamId => "\x00\x00",
+            0x1000000 > $streamId => "\x00",
+            default => ""
+        }.chr($streamId);
+
+        $payload = "";
+
+        foreach ($headers as $header) {
+            $name = pack("c", strlen($header[0])).$header[0];
+            $value = pack("c", strlen($header[1])).$header[1];
+            $payload .= chr(0x00).$name.$value;
+        }
+
+        $length = strlen($payload);
+
+        $payloadLength = match(true) {
+            0x100 > $length => "\x00\x00",
+            0x10000 > $length => "\x00",
+            default => ""
+        }.chr($length);
+
+        $flag = 0x00;
+
+        if (count($flags)) {
+           foreach ($flags as $value) {
+               $flag += $value;
+           }
+        }
+
+        $type = chr(FrameType::HEADERS);
+        $flag = pack("c", $flag);
+        $this->bytes = $payloadLength.$type.$frag.$stream.$payload;
+    }
+
+    public getBytes(): string
+    {
+        return $this->bytes;
+    }
 }
