2424886afa2e6c82f84a25b2309a9827872b8e6e
Masaki Kagaya <masakielastic@gmail.com>
2024-07-31 08:50:49 +0900

    added __construct

--- src/Frame.php
+++ src/Frame.php
@@ -5,6 +5,11 @@ namespace h2;
 class Frame {
     private $bytes;
 
+    public function __construct(string $bytes)
+    {
+        $this->bytes = $bytes;
+    }
+
     public function getBytes(): string
     {
         return $this->bytes;
