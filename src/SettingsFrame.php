a95236cd0aaefc9c31864ff225290d0ff8037f05
Masaki Kagaya <masakielastic@gmail.com>
2024-07-31 09:55:11 +0900

    fixed 4th argument

--- src/SettingsFrame.php
+++ src/SettingsFrame.php
@@ -2,9 +2,9 @@
 
 class SettingsFrame extends Frame {
 
-    public static function from()
+    public static function from(array $flags = [])
     {
-        $bytes = FrameHeader::from(0x0, 0x4, 0x0)->getBytes();
+        $bytes = FrameHeader::from(0x0, 0x4, 0x0, $flags)->getBytes();
         return new static($bytes);
     }
 }
