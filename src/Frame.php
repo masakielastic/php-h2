1b6c29cd6c4758887e59dee3f8ec0052d29aff2e
Masaki Kagaya <masakielastic@gmail.com>
2024-07-31 12:54:51 +0900

    added  in Frame

--- src/Frame.php
+++ src/Frame.php
@@ -1,17 +1,40 @@
 <?php
 
 namespace h2;
+use h2\frameHeader;
 
 class Frame {
-    private $bytes;
+    private $frameHeader;
+    private $payload;
 
     public function __construct(string $bytes)
     {
-        $this->bytes = $bytes;
+        $this->frameHeader = new FrameHeader(substr($bytes, 0, 9));
+        $this->payload = substr($bytes, 9);
+    }
+
+    public function getLength(): int
+    {
+        return $this->frameHeader->getLength();
+    }
+
+    public function getType(): int
+    {
+        return $this->frameHeader->getType();
+    }
+
+    public function getFlag(): int
+    {
+        return $this->frameHeader->getFlag();
+    }
+
+    public function getPayload():string
+    {
+        return $this->getpayload;
     }
 
     public function getBytes(): string
     {
-        return $this->bytes;
+        return $this->frameHeader->getBytes().$this->getPayload();
     }
 }
