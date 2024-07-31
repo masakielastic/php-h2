049c2c2c40bb3844cd56b4c5cb18b7fb590729a1
Masaki Kagaya <masakielastic@gmail.com>
2024-07-31 10:35:44 +0900

    added getLength, getType, getFlag

--- src/FrameInfo.php
+++ src/FrameInfo.php
@@ -4,4 +4,26 @@ namespace h2;
 
 class FrameInfo {
 
+    private $byte;
+
+    public function __construct(string $bytes)
+    {
+        $this->bytes = $bytes;
+    }
+
+
+    public function getLength()
+    {
+        return hexdec(bin2hex(substr($this->bytes, 0, 3)));
+    }
+
+    public function getType()
+    {
+        return ord(substr($this->bytes, 3, 1));
+    }
+
+     public function getFlag()
+     {
+         return ord(substr($this->bytes, 4, 1));
+     }
 }
