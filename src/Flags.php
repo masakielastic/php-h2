c8549139b20f8056b6fdcaba9648108355a9d5d3
Masaki Kagaya <masakielastic@gmail.com>
2024-07-31 22:30:38 +0900

    update Flags

--- src/Flags.php
+++ src/Flags.php
@@ -4,15 +4,15 @@ namespace h2;
 
 class Flags {
     // Data Frame
-    const DataEndStream = 0x1;
-    const DataPadded = 0x8;
+    const DATA_END_STREAM = 0x1;
+    const DATA_PADDED     = 0x8;
 
     // Headers Frame
-    const HeadersEndStream  = 0x1;
-    const HeadersEndHeaders = 0x4;
-    const HeadersPadded     = 0x8;
-    const HeadersPriority   = 0x20;
+    const HEADERS_END_STREAM  = 0x1;
+    const HEADERS_END_HEADERS = 0x4;
+    const HEADERS_PADDED      = 0x8;
+    const HEADERS_PRIORITY    = 0x20;
 
     // Settings Frame
-    const SettingsAck = 0x1;
+    const SETTINGS_ACK = 0x1;
 }
