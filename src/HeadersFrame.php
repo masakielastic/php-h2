fe5a586ae382fc4bf8940708aa23749ecd44adf8
Masaki Kagaya <masakielastic@gmail.com>
2024-07-31 10:48:30 +0900

    added payloadFromRawHeaders()

--- src/HeadersFrame.php
+++ src/HeadersFrame.php
@@ -6,15 +6,22 @@ class HeadersFrame extends Frame {
 
     public static function from(array $headers, int $streamId, array $flags = [])
     {
-         $payload = '';
-
-         foreach ($headers as $header) {
-             $name = pack("c", strlen($header[0])).$header[0];
-             $value = pack("c", strlen($header[1])).$header[1];
-             $payload .= chr(0x0).$name.$value;
-         }
+         $payload = static::payloadFromRawHeaders($headers);
 
          $header = FrameHeader::from(strlen($payload), 0x1, $streamId, $flags)->getBytes();
          return new static($header.$payload);
      }
+
+    private static function payloadFromRawHeaders(array $headers): string
+    {
+        $payload = '';
+
+        foreach ($headers as $header) {
+             $name = pack("c", strlen($header[0])).$header[0];
+             $value = pack("c", strlen($header[1])).$header[1];
+             $payload .= chr(0x0).$name.$value;
+        }
+
+        return $payload;
+    }
 }
