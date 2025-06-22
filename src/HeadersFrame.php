cd0c2eecd8d14c81705b3d4dbe84a9d090cb36a4
Masaki Kagaya <masakielastic@gmail.com>
2025-06-22 23:35:47 +0900

    added HPack encoding

--- src/HeadersFrame.php
+++ src/HeadersFrame.php
@@ -2,6 +2,8 @@
 
 namespace h2;
 
+use Amp\Http\Internal\HPackNghttp2;
+
 class HeadersFrame extends Frame {
 
     public static function from(string $payload, int $streamId, array $flags = []): static
@@ -12,21 +14,9 @@ class HeadersFrame extends Frame {
 
     public static function fromRawHeaders(array $headers, int $streamId, array $flags = []): static
     {
-        $payload = static::getPayloadFromRawHeaders($headers);
+        $hpack = new Amp\Http\Internal\HPackNghttp2;
+        $payload = $hpack->encode($headers);
         $header = static::frameHeader(strlen($payload), 0x1, $streamId, $flags);
         return new static($header.$payload);
-    }
-
-    protected static function getPayloadFromRawHeaders(array $headers): string
-    {
-        $payload = '';
-
-        foreach ($headers as $header) {
-             $name = pack("c", strlen($header[0])).$header[0];
-             $value = pack("c", strlen($header[1])).$header[1];
-             $payload .= chr(0x0).$name.$value;
-        }
-
-        return $payload;
     }
 }
